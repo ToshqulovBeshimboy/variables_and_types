bc3cb6f2aff4dcae532a12dda8272d478437b74c
ToshqulovBeshimboy <tbeshimboy@gmail.com>
2025-12-02 18:09:46 +0500

    misol bir

--- begin01.py
+++ begin01.py
@@ -1,5 +1,6 @@
 # "num_one" o'zgaruvchisiga 5 (butun son) qiymatini bering.
-
+x=5
 # "num_two" o'zgaruvchisiga 4 (butun son) qiymatini bering.
-
+y=4
 # "num_one" va "num_two" yig'indisini chop eting.
+print(x+y)
